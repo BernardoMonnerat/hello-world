cd623941a1ef1e3d26b6e5e9abd032f05c885b46
Bernardo <monneratcardozo@gmail.com>
2026-03-12 09:07:04 -0300

    meu primiro programa em java

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+
